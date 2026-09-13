6ebbfb172a6aeac48e661057f336c59ba2850efb
Peter Lourenço <lourenco.pet@gmail.com>
2026-09-13 00:21:42 -0300

    feat: Adição de modelo base de controllers para validação

---
+++ src/services/Combat/Tactician.Combat.Api/Controller/_CombatControllerBase.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tactician.Combat.Api.Controller;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CombatControllerBase : ControllerBase { }
