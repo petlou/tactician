cf5a0f01533f277a6d2f985834c7b3a83ed37791
Peter Lourenço <lourenco.pet@gmail.com>
2026-09-13 01:17:00 -0300

    feat: Criação de estrutura de pastas e arquivos do Domain

---
+++ src/services/Combat/Tactician.Combat.Domain/Common/ValueObject.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tactician.Combat.Domain.Common;
+
+internal class ValueObject
+{
+}
---
+++ src/services/Combat/Tactician.Combat.Domain/ValueObjects/HitPoints.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tactician.Combat.Domain.ValueObjects;
+
+internal class HitPoints
+{
+}
